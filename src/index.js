c1f5cded20ed88af3e1a171658b6c73356878237
Tish Chungoora <tishal82@gmail.com>
2019-11-21 14:24:24 +0000

    Fetched toys and rendered toy info on page within correct html tags with correct attributes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 let addToy = false
 
 document.addEventListener("DOMContentLoaded", ()=>{
-  const addBtn = document.querySelector('#new-toy-btn')
-  const toyForm = document.querySelector('.container')
+
+  // declare variables
+  const addBtn = document.querySelector('#new-toy-btn');
+  const toyForm = document.querySelector('.container');
+  const toyCollection = document.querySelector('#toy-collection');
+
+  // URLs
+  const API_ENDPOINT = "http://localhost:3000";
+  const TOYS_API = `${API_ENDPOINT}/toys`;
+
+  // 
   addBtn.addEventListener('click', () => {
     // hide & seek with the form
     addToy = !addToy
@@ -12,5 +21,49 @@ document.addEventListener("DOMContentLoaded", ()=>{
       toyForm.style.display = 'none'
     }
   })
+
+  // functions to run
+  loadToys();
+
+  // fetch object and render characters
+  function loadToys() {
+    return fetch(TOYS_API)
+      .then(function(response){
+        return response.json();
+      })
+      .then(function(toysJson){
+        renderToys(toysJson);
+      })
+  }
+
+  // render characters
+  function renderToys(toysJson) {
+    toysJson.forEach(function(character) {
+      let toyDiv = document.createElement("div");
+      let toyName = document.createElement("h2");
+      let toyImg = document.createElement("img");
+      let toyLikes = document.createElement("p");
+      let likeBtn = document.createElement("button");
+      
+      toyDiv.setAttribute("class", "card");
+      toyImg.setAttribute("class", "toy-avatar");
+      likeBtn.setAttribute("class", "like-btn");
+
+      toyName.innerText = character.name;
+      toyImg.src = character.image;
+      toyLikes.innerText = `${character.likes} Like(s)`;
+      likeBtn.innerText = "Like ♡";
+      
+      toyDiv.appendChild(toyName);
+      toyDiv.appendChild(toyImg);
+      toyDiv.appendChild(toyLikes);
+      toyDiv.appendChild(likeBtn);
+      toyCollection.appendChild(toyDiv);
+    })
+  }
+
+
+
+
 
 })
